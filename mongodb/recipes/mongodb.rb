303d4695828ba42fb036d0f14547e07bc2eb9f1d
manibrar <manvirbrar1988@gmail.com>
2018-07-27 15:22:18 +0100

    unit and integration tests

--- mongodb/recipes/mongodb.rb
+++ mongodb/recipes/mongodb.rb
@@ -1,8 +1,9 @@
-include_recipe 'mongodb::default'
-
+apt_update 'update' do
+  action :update
+end
 
 #Mongodb-org added to source list
-apt_repository "mongodb-org" do
+apt_repository "mongodb" do
   uri "http://repo.mongodb.org/apt/ubuntu"
   distribution "xenial/mongodb-org/3.2"
   components ["multiverse"]
@@ -12,5 +13,27 @@ end
 
 #Install and upgrade
 package "mongodb-org" do
-  action :install
+  action [ :install, :upgrade ]
+end
+
+service 'mongod' do
+  supports status: true, restart: true, reload: true
+  action [ :enable, :start ]
+end
+
+template '/lib/systemd/system/mongod.service' do
+  source 'mongod.service.erb'
+  mode '0600'
+  owner 'root'
+  group 'root'
+  notifies :restart, 'service[mongod]'
+end
+
+template '/etc/mongod.conf' do
+  variables port: node['mongo']['port'], ip_address: node['mongo']['ip_address']
+  source 'mongod.conf.erb'
+  mode '0755'
+  owner 'root'
+  group 'root'
+  notifies :restart, 'service[mongod]'
 end
--- mongodb/spec/unit/recipes/mongodb_spec.rb
+++ mongodb/spec/unit/recipes/mongodb_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'mongo::mongodb' do
+describe 'mongodb::mongodb' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
@@ -8,8 +8,30 @@ describe 'mongo::mongodb' do
       runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
-    it 'should install mongodb-org' do
-      expect(chef_run).to install_package 'mongodb-org'
+    it 'should update' do
+      expect(chef_run).to update_apt_update 'update'
+    end
+    it 'should add mongo to the source list' do
+      expect(chef_run).to add_apt_repository('mongodb')
+    end
+    it 'should install mongod and upgrade' do
+      expect(chef_run).to upgrade_package 'mongodb-org'
+    end
+    it 'should enable service mongod' do
+      expect(chef_run).to enable_service 'mongod'
+    end
+    it 'should start service mongod' do
+      expect(chef_run).to start_service 'mongod'
+    end
+    it 'should update the mongo service config' do
+      expect(chef_run).to create_template('/lib/systemd/system/mongod.service').with(source: 'mongod.service.erb')
+      template = chef_run.template('/lib/systemd/system/mongod.service')
+      expect(template).to notify('service[mongod]')
+    end
+    it 'should update the mongod config' do
+      expect(chef_run).to create_template('/etc/mongod.conf').with_variables(port: 27017, ip_address: ['0.0.0.0'])
+      template = chef_run.template('/etc/mongod.conf')
+      expect(template).to notify('service[mongod]')
     end
       at_exit { ChefSpec::Coverage.report! }
   end
